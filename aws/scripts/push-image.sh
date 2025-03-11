0c308917d06776097fd99ef14e9af6f519571433
Anthony Appau <anthony.appau@abtglobal.com>
2025-03-11 17:57:18 -0400

    rocky/shiny:4.4.1 to ECR to avoid docker hub timeout

--- aws/scripts/push-image.sh
+++ aws/scripts/push-image.sh
@@ -5,7 +5,7 @@ if [ $# -eq 0 ]; then
 else
     aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
     if [ "$1" == "base" ]; then
-        docker build --provenance=false -t eva/base -f aws/docker/Dockerfile.Base .
+        docker build --provenance=false --build-arg BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/shiny:$SHINY_VERSION -t eva/base -f aws/docker/Dockerfile.Base .
         docker tag eva/base:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base:latest
         docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base:latest
     fi
