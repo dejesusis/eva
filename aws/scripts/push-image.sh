ec926a614e3f2b086647fc52d639f6c2e6c820f2
Anthony Appau <anthony.appau@abtglobal.com>
2025-04-16 19:54:48 -0400

    splitting base image for different environments

--- aws/scripts/push-image.sh
+++ aws/scripts/push-image.sh
@@ -5,12 +5,12 @@ if [ $# -eq 0 ]; then
 else
     aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
     if [ "$1" == "base" ]; then
-        docker build --provenance=false --build-arg BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/shiny:$SHINY_VERSION -t eva/base -f aws/docker/Dockerfile.Base .
-        docker tag eva/base:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base:latest
-        docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base:latest
+        docker build --provenance=false --build-arg BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/shiny:$SHINY_VERSION -t eva/base-$ENVIRONMENT -f aws/docker/Dockerfile.Base .
+        docker tag eva/base-$ENVIRONMENT:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base-$ENVIRONMENT:latest
+        docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base-$ENVIRONMENT:latest
     fi
     if [ "$1" == "app" ]; then
-        docker build --provenance=false --build-arg APP_ENV=$ENVIRONMENT --build-arg BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base:latest -t eva/app-$ENVIRONMENT -f aws/docker/Dockerfile.App .
+        docker build --provenance=false --build-arg APP_ENV=$ENVIRONMENT --build-arg BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/base-$ENVIRONMENT:latest -t eva/app-$ENVIRONMENT -f aws/docker/Dockerfile.App .
         docker tag eva/app-$ENVIRONMENT:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/app-$ENVIRONMENT:latest
         docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/eva/app-$ENVIRONMENT:latest
     fi
